49a4b7548782ea48c5e8c4771d6edb577bcf5eda
unknown <angelpalacios01234@gmail.com>
2023-03-28 19:04:49 -0300

    factorial1.py, factorial2.py, factorial3.py y factorial4.py

--- src/factorial/factorial4.py
+++ src/factorial/factorial4.py
@@ -4,7 +4,7 @@
 #* Creative commons                                                        *
 #*-------------------------------------------------------------------------*
 import sys
-#nombre del programa: factorial de los numeros entre dos extremos [a, b]
+#nombre del programa: factorial de los numeros entre dos extremos [1, b] sin limite superior
 def factorial(num): 
     if num < 0: 
         print("Factorial de un nÃºmero negativo no existe")
@@ -24,17 +24,21 @@ if len(sys.argv) == 0:
    print("Debe informar un nÃºmero!")
    sys.exit()
 '''
-if len(sys.argv) < 3:
-   nums= input("Ingrese dos numeros(separados por un espacio): ")
-   nums = nums.split(" ")
-   a=int(nums[0])
-   b=int(nums[1])
-else:
-    #num=int(sys.argv[1])
-    a=int(sys.argv[1])
-    b=int(sys.argv[2])
+print(sys.argv)
+lim_inf = 1
+lim_sup = 60
 
-#print(sys.argv)
+a= lim_inf
+b= lim_sup
+if len(sys.argv) == 1:
+   a= int(input("Ingrese el limite inferior(mayor o igual a 1): "))
+else:
+    a=int(sys.argv[1])
+
+if(a<lim_inf):
+       a= lim_inf
+
+print(sys.argv)
 #num=int(sys.argv[1])
 print('a=',a,", b=",b)
 
