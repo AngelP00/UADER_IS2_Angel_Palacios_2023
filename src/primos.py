ac2f4f46fac4c9366fe166c98c9042929d4da2ea
unknown <angelpalacios01234@gmail.com>
2023-03-28 15:52:34 -0300

    se agregaron comentarios al programa primos.py

--- src/primos.py
+++ src/primos.py
@@ -1,6 +1,7 @@
 max = int(input("Find primes up to what number? : "))
 primeList = []
 #for loop for checking each number
+#bucle for para verificar cada numero
 for x in range(2, max + 1):
 	isPrime = True
 	index = 0
@@ -10,11 +11,13 @@ for x in range(2, max + 1):
 			isPrime = False
 			break
 		index += 1
+	#si es el numero es primo se agrega a la lista de numemros primos
 	if isPrime:
 		primeList.append(x)
 print(primeList)
 #-------------------------------------------------------------
 # prime number calculator: find the first n primes
+# calculadora de numeros primos: encuentra los primeros n primos
 count = int(input("Find how many primes?: "))
 primeList = []
 x = 2
